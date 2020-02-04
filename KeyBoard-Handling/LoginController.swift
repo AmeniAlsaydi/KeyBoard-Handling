5a3d0e0cf957aaae4c0cd19acbf06291b21b067e
Ameni Alsaydi <amenialsaydi@pursuit.org>
2020-02-04 16:30:17 -0500

    set the current VC as an observer for notifications from the observer

--- KeyBoard-Handling/LoginController.swift
+++ KeyBoard-Handling/LoginController.swift
@@ -16,7 +16,11 @@ class LoginController: UIViewController {
     @IBOutlet weak var userTextFeild: UITextField!
     @IBOutlet weak var passwordLabel: UILabel!
     @IBOutlet weak var passwordTextfield: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
     
+    override func viewWillLayoutSubviews() {
+        loginButton.layer.cornerRadius = 10
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,7 +34,7 @@ class LoginController: UIViewController {
     
     private func registerForKeyboardNotifcations() {
         // singleton:
-        // add ourselevs ass observer
+        // set the current VC as an observer for notifications from the observer 
         NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(_:)), name: UIResponder.keyboardWillShowNotification, object: nil)
         
          NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide(_:)), name: UIResponder.keyboardWillHideNotification, object: nil)
@@ -39,11 +43,13 @@ class LoginController: UIViewController {
     
     @objc func keyboardWillShow(_ notification: NSNotification) {
         print("keyboard shown")
+        // here is where we animate the VC and push everyhting up away from the key board.
     }
     
     @objc func keyboardWillHide(_ notification: NSNotification) {
         print("keyboard hidden")
-        
+        // here is where we animate the VC and push everyhting back down
+
     }
 
 }
@@ -53,3 +59,5 @@ extension LoginController: UITextFieldDelegate {
 }
 
 // note: save all the constraints that you want to alter in an array and map through it to animate it up.
+
+// maybe add a tap gesture that resigns the key board if the view is touched.
